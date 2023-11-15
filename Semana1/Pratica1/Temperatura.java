4ac0cd9ac148cd3f8fccfa4b2e40190e7f5ff7cd
Leitetc <thiagoleite22@hotmail.com>
2023-11-15 12:19:53 -0300

    commit

--- Semana1/Pratica1/Temperatura.java
+++ Semana1/Pratica1/Temperatura.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ConversorTemperatura {
+public class Temperatura {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
